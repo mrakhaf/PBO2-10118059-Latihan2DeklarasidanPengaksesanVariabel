a7f149b25ae1c9bed611df06b311385c076cb1a0
mrakhaf <48128093+mrakhaf@users.noreply.github.com>
2019-09-26 17:00:40 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118059/latihan2deklarasidanpengaksesanvariabel/PBO210118059Latihan2DeklarasidanPengaksesanVariabel.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118059.latihan2deklarasidanpengaksesanvariabel;
+
+/**
+ *
+ * @author 
+ * Nama  : Muhammad Rakha Firdaus
+ * NIM   : 10118059
+ * Kelas : IF-2
+ * 
+ */
+public class PBO210118059Latihan2DeklarasidanPengaksesanVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //deklarasiVariabel
+        int nilaiInt;
+        final double PHI = 3.14; //cons uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        //Memberi Nilai ke Variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        //Menampilkan Hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " + PHI);
+        System.out.println("Isi variabel logika = " + nilaiLogika);
+        System.out.println("Isi variabel karakter = " + nilaiKarakter);
+        
+    }
+    
+}
